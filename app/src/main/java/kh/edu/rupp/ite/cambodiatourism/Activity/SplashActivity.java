3e78856c0bf962f945b0e1b59f5863849072d3be
HeangHenglong <henglong.ite@gmail.com>
2023-10-27 19:31:36 +0700

    Final Project

--- app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/SplashActivity.java
+++ app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/SplashActivity.java
@@ -1,27 +1,29 @@
 package kh.edu.rupp.ite.cambodiatourism.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 import kh.edu.rupp.ite.cambodiatourism.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_DELAY = 3000;// 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // Delay for a few seconds before launching the main activity
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }, SPLASH_DELAY);
+        ConstraintLayout introBtn = findViewById(R.id.introBtn);
+        introBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        });
     }
 }
