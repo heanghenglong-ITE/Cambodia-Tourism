2c00065cd23c4586ed0aacedb5669c9ebb07e123
Chum Tochhit <kdmahmab1@gmail.com>
2023-11-03 14:48:01 +0700

    Final Project

---
+++ app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/Hospital.java
@@ -0,0 +1,14 @@
+package kh.edu.rupp.ite.cambodiatourism.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import kh.edu.rupp.ite.cambodiatourism.R;
+
+public class Hospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital);
+    }
+}
