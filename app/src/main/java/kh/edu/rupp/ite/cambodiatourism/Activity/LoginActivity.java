d4b9c09db65582e5e9f08085cfc8b31529259685
Henglong <pozzlong1998@gmail.com>
2024-01-06 12:56:47 +0700

    Final Project

--- app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/LoginActivity.java
+++ app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/LoginActivity.java
@@ -3,8 +3,12 @@ package kh.edu.rupp.ite.cambodiatourism.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import kh.edu.rupp.ite.cambodiatourism.R;
@@ -24,6 +28,37 @@ public class LoginActivity extends AppCompatActivity {
         binding.buttonLogin.setOnClickListener(v -> navigateToMain());
 
 
+        TextView textViewClick = findViewById(R.id.register_type);
+        ImageView imageViewClickFb = findViewById(R.id.facebook);
+        ImageView imageViewClickIg = findViewById(R.id.google);
+        ImageView imageViewClickTt = findViewById(R.id.twitter);
+        textViewClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToAnotherActivity();
+            }
+        });
+        imageViewClickFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFacebook();
+            }
+        });
+
+        imageViewClickIg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInstragram();
+            }
+        });
+
+        imageViewClickTt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTwitter();
+            }
+        });
+
     }
 
     private void navigateToMain() {
@@ -40,5 +75,25 @@ public class LoginActivity extends AppCompatActivity {
                 binding.buttonLogin.setEnabled(false);
             }
         }
+    }
+    private void navigateToAnotherActivity(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    private void openFacebook(){
+        Uri uri = Uri.parse("https://web.facebook.com/login/?_rdc=1&_rdr");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    private void openInstragram(){
+        Uri uri = Uri.parse("https://web.facebook.com/login/?_rdc=1&_rdr");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    private void openTwitter(){
+        Uri uri = Uri.parse("https://web.facebook.com/login/?_rdc=1&_rdr");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
--- app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/RegisterActivity.java
+++ app/src/main/java/kh/edu/rupp/ite/cambodiatourism/Activity/RegisterActivity.java
@@ -2,7 +2,10 @@ package kh.edu.rupp.ite.cambodiatourism.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import kh.edu.rupp.ite.cambodiatourism.R;
 
@@ -12,5 +15,18 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView textViewClick = findViewById(R.id.login_type);
+        textViewClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToLoginActivity();
+            }
+        });
+
+    }
+    private void navigateToLoginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
